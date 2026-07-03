df24911ef17a338c1744be2245703171bb4c3380
Roseni <roseniarjun@gmail.com>
2026-07-03 20:24:40 +0530

    Update search feature and ecommerce flow

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,29 +11,92 @@ import {
 } from "react-icons/fi";
 import { FaWhatsapp } from "react-icons/fa";
 import { CartContext } from "../context/CartContext";
+import { products } from "../data/products"
+
 
 
 export default function Navbar() {
   const [open, setOpen] = useState(false);
   const [activeTab, setActiveTab] = useState("menu");
- 
-  const { 
+
+
+
+
+
+  const {
     cart,
-  removeFromCart,
-increaseQuantity,
-decreaseQuantity } = useContext(CartContext)
+    removeFromCart,
+    increaseQuantity,
+    decreaseQuantity } = useContext(CartContext)
 
-const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
+  const totalItems = cart.reduce((sum, item) => sum + item.quantity, 0);
 
-const cartTotal = cart.reduce(
-  (sum, item) => sum + item.price * item.quantity,
-  0
-);
+  const cartTotal = cart.reduce(
+    (sum, item) => sum + item.price * item.quantity,
+    0
+  );
 
 
-  const [cartOpen, setCartOpen]=useState(false)
+  const [cartOpen, setCartOpen] = useState(false)
 
-  const navigate=useNavigate();
+  const navigate = useNavigate();
+
+
+  const [search, setSearch] = useState("");
+   const [filteredProducts, setFilteredProducts] = useState([])
+
+  // console.log(filteredProducts);
+
+  //
+
+  const handleSearch = (e) => {
+    const value = e.target.value;
+
+    setSearch(value);
+
+    if (value.trim() === "") {
+      setFilteredProducts([]);
+      return;
+    }
+    const result = products.filter((item) =>
+      item.name.toLowerCase().includes(value.toLowerCase())
+    );
+
+    setFilteredProducts(result);
+  }
+
+   const [searchOpen,setSearchOpen]=useState(false)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   return (
     <>
@@ -69,8 +132,64 @@ const cartTotal = cart.reduce(
             <input
               type="text"
               placeholder="Search here for all products"
-            />
-            <button className="search-btn">
+              onFocus={()=>setSearchOpen(true)}
+              />
+              {searchOpen && (
+  <div className="search-overlay">
+    <div className="search-modal">
+
+      <div className="search-modal-top">
+        <div className="search-modal-box">
+          <FiSearch />
+          <input
+            type="text"
+            placeholder="Search products..."
+            value={search}
+            onChange={handleSearch}
+            autoFocus
+          />
+        </div>
+
+        <button
+          className="search-close"
+          onClick={() => {
+            setSearchOpen(false);
+            setSearch("");
+            setFilteredProducts([]);
+          }}
+        >
+          ×
+        </button>
+      </div>
+
+      <div className="search-modal-grid">
+        {filteredProducts.map((item) => (
+          <div
+            className="search-product-card"
+            key={item.id}
+            onClick={() => {
+              navigate(`/product/${item.id}`);
+              setSearchOpen(false);
+              setSearch("");
+              setFilteredProducts([]);
+            }}
+          >
+            <img src={item.image} alt={item.name} />
+            <h4>{item.name}</h4>
+            <p>Rs. {item.price}</p>
+          </div>
+        ))}
+      </div>
+
+      {search && filteredProducts.length === 0 && (
+        <p className="no-search">No products found</p>
+      )}
+
+    </div>
+  </div>
+)}
+             
+            <button className="search-btn" onClick={()=> setSearchOpen(true)}>
               <FiSearch />
             </button>
           </div>
@@ -95,12 +214,12 @@ const cartTotal = cart.reduce(
             </button>
 
             <button
-            className="icon-button"
-            style={{position: "relative"}}
-            onClick={()=>setCartOpen(true)}>
-              <FiShoppingCart/>
+              className="icon-button"
+              style={{ position: "relative" }}
+              onClick={() => setCartOpen(true)}>
+              <FiShoppingCart />
 
-              {totalItems>0 && (
+              {totalItems > 0 && (
                 <span className="cart-count">{totalItems}</span>
               )}
 
@@ -110,76 +229,79 @@ const cartTotal = cart.reduce(
       </header>
 
       {cartOpen && (
-  <>
-    <div
-      className="cart-overlay"
-      onClick={() => setCartOpen(false)}
-    ></div>
+        <>
+          <div
+            className="cart-overlay"
+            onClick={() => setCartOpen(false)}
+          ></div>
 
-    <div className="cart-drawer">
-      <div className="cart-drawer-header">
-        <h3>Shopping Cart ({totalItems})</h3>
-        <button onClick={() => setCartOpen(false)}>
-          ×
-        </button>
-      </div>
-
-      <div className="cart-drawer-items">
-        {cart.map((item) => (
-          <div className="cart-drawer-item" key={`${item.id}-${item.size}`}>
-            <img src={item.image} alt={item.name}  
-             onClick={()=>{setCartOpen(false)
-              navigate(`/product/${item.id}`)
-            }}
-            style={{cursor:"pointer"}}
-            />
-
-            <div>
-              <h4>{item.name}</h4>
-              <p>Size: {item.size}</p>
-              <strong>₹{item.price}</strong>
-
-              <div className="drawer-qty">
-                <button onClick={() => decreaseQuantity(item.id, item.size)}>
-                  -
-                </button>
-                <span>{item.quantity}</span>
-                <button onClick={() => increaseQuantity(item.id, item.size)}>
-                  +
-                </button>
-              </div>
+          <div className="cart-drawer">
+            <div className="cart-drawer-header">
+              <h3>Shopping Cart ({totalItems})</h3>
+              <button onClick={() => setCartOpen(false)}>
+                ×
+              </button>
             </div>
 
-            <button
-              className="drawer-remove"
-              onClick={() => removeFromCart(item.id, item.size)}
-            >
-              Remove
-            </button>
+            <div className="cart-drawer-items">
+              {cart.map((item) => (
+                <div className="cart-drawer-item" key={`${item.id}-${item.size}`}>
+                  <img src={item.image} alt={item.name}
+                    onClick={() => {
+                      setCartOpen(false)
+                      navigate(`/product/${item.id}`)
+                    }}
+                    style={{ cursor: "pointer" }}
+                  />
+
+                  <div>
+                    <h4>{item.name}</h4>
+                    <p>Size: {item.size}</p>
+                    <strong>₹{item.price}</strong>
+
+                    <div className="drawer-qty">
+                      <button onClick={() => decreaseQuantity(item.id, item.size)}>
+                        -
+                      </button>
+                      <span>{item.quantity}</span>
+                      <button onClick={() => increaseQuantity(item.id, item.size)}>
+                        +
+                      </button>
+                    </div>
+                  </div>
+
+                  <button
+                    className="drawer-remove"
+                    onClick={() => removeFromCart(item.id, item.size)}
+                  >
+                    Remove
+                  </button>
+                </div>
+              ))}
+            </div>
+
+            <div className="cart-drawer-footer">
+              <div>
+                <span>Subtotal</span>
+                <strong>₹{cartTotal}</strong>
+              </div>
+
+              <button onClick={() => {
+                setCartOpen(false)
+                navigate("/cart")
+              }}>
+                VIEW CART
+              </button>
+              <button onClick={() => {
+                setCartOpen(false)
+                navigate("/checkout")
+              }}>
+                CHECKOUT
+              </button>
+            </div>
           </div>
-        ))}
-      </div>
-
-      <div className="cart-drawer-footer">
-        <div>
-          <span>Subtotal</span>
-          <strong>₹{cartTotal}</strong>
-        </div>
-
-        <button onClick={() => {
-          setCartOpen(false)
-          navigate("/cart")}}>
-          VIEW CART
-        </button>
-        <button onClick={() => {
-          setCartOpen(false)
-          navigate("/checkout")}}>
-          CHECKOUT
-        </button>
-      </div>
-    </div>
-  </>
-)}
+        </>
+      )}
 
 
 
